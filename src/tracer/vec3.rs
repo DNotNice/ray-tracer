bf2129f3cc46e1e8398d778b3e574ac91548b40b
DNotNice <dewanshpandey@gmail.com>
2024-10-04 19:39:18 +0530

    added more funcs to vec class

--- src/tracer/vec3.rs
+++ src/tracer/vec3.rs
@@ -1,4 +1,5 @@
-
+use core::fmt;
+use std::ops::{Add, Div, Mul, Neg, Sub} ;
 
 #[derive(Clone, Copy, Default)]
 pub struct Vec3{
@@ -27,5 +28,96 @@ impl Vec3{
         let length = self.length();
         Vec3::new(self.x/length , self.y/length , self.z /length)
     }
+    pub fn write_colour(self) {
+        let i_r = (256.0*self.x).round() as u8;
+        let i_g = (256.0*self.y).round() as u8;
+        let i_b = (256.0*self.z).round() as u8;
+        print!("{} {} {}\n", i_r, i_g, i_b)
+    }
+    pub fn cross(mut self, other: Vec3) -> Vec3 {
+        Vec3 {
+            x: self.y * other.z - self.z * other.y,
+            y: self.z * other.x - self.x * other.z,
+            z: self.x * other.y - self.y * other.x,
+        }
+    }
 
 }
+
+impl Add for Vec3 {
+    type Output = Vec3;
+    
+    fn add(self, rhs: Vec3  ) -> Vec3 {
+        Vec3 { 
+               x: self.x + rhs.x ,
+               y: self.y + rhs.y ,
+               z: self.z + rhs.z 
+            }
+    }
+}
+
+impl Mul<f64> for Vec3 {
+    type Output = Vec3;
+    
+    fn mul(self, rhs: f64) -> Vec3 {
+        Vec3 { 
+               x: self.x * rhs ,
+               y: self.y * rhs ,
+               z: self.z * rhs 
+            }
+    }
+}
+
+impl Mul<Vec3> for Vec3 {
+    type Output = Vec3;
+    
+    fn mul(self, rhs: Vec3) -> Vec3 {
+        Vec3 { 
+               x: self.x * rhs.x ,
+               y: self.y * rhs.y ,
+               z: self.z * rhs.z 
+            }
+    }
+}
+
+impl Div<f64> for Vec3 {
+    type Output = Vec3;
+    
+    fn div(self, rhs: f64) -> Vec3 {
+        Vec3 {
+               x: self.x / rhs ,
+               y: self.y / rhs ,
+               z: self.z / rhs 
+            }
+    }
+}
+
+impl Sub for Vec3 {
+    type Output = Vec3;
+    
+    fn sub(self, rhs: Vec3) -> Vec3 {
+        Vec3 { 
+               x: self.x - rhs.x ,
+               y: self.y - rhs.y ,
+               z: self.z - rhs.z 
+            }
+    }
+}
+
+impl Neg for Vec3 {
+    type Output = Vec3;
+    fn neg(self) -> Vec3 {
+        Vec3 {
+            x: -&self.x,
+            y: -&self.y,
+            z: -&self.z,
+        }
+    }
+}
+
+impl fmt::Display for Vec3 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({} ,{} , {})" ,self.x , self.y , self.z)
+    }
+}
+
